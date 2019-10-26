f9c7c918cec6a2e743a678254534d761fefacf65
akikgt <komoria@oregonstate.edu>
2019-10-26 13:33:23 +0900

    Add memcmp.s

--- src/modules/real/memcmp.s
+++ src/modules/real/memcmp.s
@@ -9,23 +9,38 @@ memcpy:
     ;----------------------
     ; save registers
     ;----------------------
+        push    bx
         push    cx
+        push    dx
         push    si
         push    di
 
     ;----------------------
-    ; copy bytes
+    ; get parameters
     ;----------------------
         cld
         mov     di, [bp + 4]
         mov     si, [bp + 6]
         mov     cx, [bp + 8]
 
-        rep movsb
-
+    ;----------------------
+    ; compare
+    ;----------------------
+        repe cmpsb
+        jnz     .10F
+        mov     ax, 0
+        jmp     .10E
+.10F:
+        mov     ax, -1
+.10E:
+    ;----------------------
+    ; restore registers
+    ;----------------------
         pop     di
         pop     si
+        pop     dx
         pop     cx
+        pop     bx
 
         mov     sp, bp
         pop     bp
