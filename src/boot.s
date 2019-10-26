bab8a5649fa4e11c5a689ef7ab14b3efbbeac50a
akikgt <komoria@oregonstate.edu>
2019-10-26 13:44:42 +0900

    ch14.3

--- src/boot.s
+++ src/boot.s
@@ -1,5 +1,36 @@
+        BOOT_LOAD       equ     0x7c00
+        ORG         BOOT_LOAD
 entry:
-        jmp     $
+        jmp     ipl
 
+        ; BPB (BIOS Parameter Block)
+        times   90 - ($ -$$) db 0x90
+
+
+        ;--------------------
+        ; IPL (Initial Program Loader)
+        ;--------------------
+ipl:
+        cli     ; forbid interupt
+
+        mov     ax, 0x0000
+        mov     ds, ax
+        mov     es, ax
+        mov     ss, ax
+        mov     sp, BOOT_LOAD
+
+        sti     ; permit interupt
+
+        mov     [BOOT.DRIVE], dl    ;save boot drive. BIOS saves drive num to dl
+
+        ;--------------------
+        ; loading end 
+        ;--------------------
+        jmp     $
+ALIGN 2, db 0
+BOOT:
+.DRIVE:         dw  0       ; drive number
+
+; End of first 512bytes
         times 510 - ($ - $$) db 0x00
         db  0x55, 0xAA
